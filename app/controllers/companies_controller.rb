bd0253be2f80f4ea7c63171105e3a10d2fa58c44
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-09-27 19:08:21 -0300

    Fim da Aula 6

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,31 @@
+class CompaniesController < ApplicationController
+
+  def new
+    # current_user.build_company ja cria uma empresa para o usuario (current_user)
+    @company = current_user.build_company
+  end
+
+  def edit
+    @company = current_user.company
+  end
+
+  def create
+    @company = current_user.build_company(company_params)
+
+    if @company.save
+      flash[:notice] = 'Empresa cadastrada com sucesso.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Erro ao cadastrar a empresa.'
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private
+    def company_params
+      params.require(:company).permit(:name, :url, :logo)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   devise_for :users
+
+  resources :companies, only: [:new, :edit, :create, :update]
+  resources :positions
+  resources :applicants, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
