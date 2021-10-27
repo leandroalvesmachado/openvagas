d9889238614cebc35a1274480eb4edac10447cc6
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-10-27 19:44:05 -0300

    Fim da aula 8

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,7 +1,7 @@
 module CompaniesHelper
   
   def dynamic_url_company
-    if current_user.company.present?
+    if current_user.company.present? && current_user.company.try(:id).present?
       edit_company_path(current_user.company)
     else
       new_company_path
